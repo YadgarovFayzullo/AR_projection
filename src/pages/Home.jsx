3ba7f6d58b0d4b0b0ab97125ba4e48097aa04ae9
Fayzullo <yadgarovfayzullo0@gmail.com>
2024-05-07 15:32:47 +0500

    second

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,12 +7,12 @@ import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <>
+    <div>
       <Navbar />
       <Header />
       <About />
-      <Download/>
-      <Footer/>
-    </>
+      <Download />
+      <Footer />
+    </div>
   );
 }
